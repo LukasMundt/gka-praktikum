68bcf0ca8dd0f723c44c8427e7f05b65f2391a80
Lukas Mundt <mail@lukas-mundt.de>
2025-11-10 17:43:08 +0100

    move the visualizing to an extra class

--- src/main/java/haw/gka/praktikum/GraphVisualizer.java
+++ src/main/java/haw/gka/praktikum/GraphVisualizer.java
@@ -1,8 +1,39 @@
 package haw.gka.praktikum;
 
-/**
- * visualizing of graphs by using JGraphT or GraphViz or other lib
- */
+import org.graphstream.graph.EdgeRejectedException;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 
 public class GraphVisualizer {
+
+    public static void displayGraph(GraphModel graph) {
+        System.setProperty("org.graphstream.ui", "javafx");
+
+        Graph graphViz = new SingleGraph("GKA Praktikum");
+
+        graph.getNodes().forEach(node -> {
+            graphViz.addNode(node.getName());
+        });
+
+
+        graph.getEdges().forEach(edge -> {
+            String idFrom = edge.getStart().getName();
+            String idTo = edge.getEnd().getName();
+            try {
+                graphViz.addEdge(idFrom+idTo, idFrom, idTo, edge.isDirected());
+            } catch (EdgeRejectedException exception) {
+                System.out.println(exception.getMessage());
+            }
+        });
+
+        // add ui labels
+        graphViz.nodes().forEach(n -> n.setAttribute("ui.label", n.getId()));
+        graphViz.edges().forEach(edge -> edge.setAttribute("ui.label", edge.getId()));
+
+        graphViz.setAttribute("ui.stylesheet",
+                "node { fill-color: #1E88E5; size: 20px; text-alignment: above; text-size: 14px; } " +
+                        "edge { fill-color: #999; size: 2px; }");
+
+        graphViz.display();
+    }
 }
--- src/main/java/haw/gka/praktikum/MainApplication.java
+++ src/main/java/haw/gka/praktikum/MainApplication.java
@@ -1,26 +1,13 @@
 package haw.gka.praktikum;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import org.graphstream.graph.EdgeRejectedException;
-import org.graphstream.graph.Graph;
-import org.graphstream.graph.implementations.SingleGraph;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainApplication extends Application {
-    public void start(Stage primaryStage) {
-    }
+    public void start(Stage primaryStage) {}
 
     public static void main(String[] args) {
         Set<Node> nodes = new HashSet<>();
@@ -38,63 +25,41 @@ public class MainApplication extends Application {
 
         Set<Edge> edges = new HashSet<>();
 
-        edges.add(new Edge(Node.getNode("c"), Node.getNode("d"), false));
-        edges.add(new Edge(Node.getNode("d"), Node.getNode("f"), false));
-        edges.add(new Edge(Node.getNode("d"), Node.getNode("e"), false));
-        edges.add(new Edge(Node.getNode("d"), Node.getNode("g"), false));
-        edges.add(new Edge(Node.getNode("e"), Node.getNode("j"), false));
-        edges.add(new Edge(Node.getNode("e"), Node.getNode("c"), false));
-        edges.add(new Edge(Node.getNode("e"), Node.getNode("e"), false));
-        edges.add(new Edge(Node.getNode("e"), Node.getNode("f"), false));
-        edges.add(new Edge(Node.getNode("f"), Node.getNode("a"), false));
-        edges.add(new Edge(Node.getNode("f"), Node.getNode("g"), false));
-        edges.add(new Edge(Node.getNode("f"), Node.getNode("h"), false));
-        edges.add(new Edge(Node.getNode("f"), Node.getNode("i"), false));
-        edges.add(new Edge(Node.getNode("g"), Node.getNode("g"), false));
-        edges.add(new Edge(Node.getNode("g"), Node.getNode("e"), false));
-        edges.add(new Edge(Node.getNode("g"), Node.getNode("b"), false));
-        edges.add(new Edge(Node.getNode("g"), Node.getNode("d"), false));
-        edges.add(new Edge(Node.getNode("h"), Node.getNode("b"), false));
-        edges.add(new Edge(Node.getNode("h"), Node.getNode("c"), false));
-        edges.add(new Edge(Node.getNode("h"), Node.getNode("f"), false));
-        edges.add(new Edge(Node.getNode("i"), Node.getNode("a"), false));
-        edges.add(new Edge(Node.getNode("i"), Node.getNode("c"), false));
-        edges.add(new Edge(Node.getNode("i"), Node.getNode("i"), false));
-        edges.add(new Edge(Node.getNode("i"), Node.getNode("g"), false));
-        edges.add(new Edge(Node.getNode("j"), Node.getNode("k"), false));
-        edges.add(new Edge(Node.getNode("j"), Node.getNode("c"), false));
-        edges.add(new Edge(Node.getNode("j"), Node.getNode("a"), false));
-        edges.add(new Edge(Node.getNode("j"), Node.getNode("b"), false));
-        edges.add(new Edge(Node.getNode("k"), Node.getNode("c"), false));
-        edges.add(new Edge(Node.getNode("k"), Node.getNode("g"), false));
-        edges.add(new Edge(Node.getNode("k"), Node.getNode("d"), false));
+        edges.add(new Edge(Node.getNode("c"), Node.getNode("d"), true));
+        edges.add(new Edge(Node.getNode("d"), Node.getNode("f"), true));
+        edges.add(new Edge(Node.getNode("d"), Node.getNode("e"), true));
+        edges.add(new Edge(Node.getNode("d"), Node.getNode("g"), true));
+        edges.add(new Edge(Node.getNode("e"), Node.getNode("j"), true));
+        edges.add(new Edge(Node.getNode("e"), Node.getNode("c"), true));
+        edges.add(new Edge(Node.getNode("e"), Node.getNode("e"), true));
+        edges.add(new Edge(Node.getNode("e"), Node.getNode("f"), true));
+        edges.add(new Edge(Node.getNode("f"), Node.getNode("a"), true));
+        edges.add(new Edge(Node.getNode("f"), Node.getNode("g"), true));
+        edges.add(new Edge(Node.getNode("f"), Node.getNode("h"), true));
+        edges.add(new Edge(Node.getNode("f"), Node.getNode("i"), true));
+        edges.add(new Edge(Node.getNode("g"), Node.getNode("g"), true));
+        edges.add(new Edge(Node.getNode("g"), Node.getNode("e"), true));
+        edges.add(new Edge(Node.getNode("g"), Node.getNode("b"), true));
+        edges.add(new Edge(Node.getNode("g"), Node.getNode("d"), true));
+        edges.add(new Edge(Node.getNode("h"), Node.getNode("b"), true));
+        edges.add(new Edge(Node.getNode("h"), Node.getNode("c"), true));
+        edges.add(new Edge(Node.getNode("h"), Node.getNode("f"), true));
+        edges.add(new Edge(Node.getNode("i"), Node.getNode("a"), true));
+        edges.add(new Edge(Node.getNode("i"), Node.getNode("c"), true));
+        edges.add(new Edge(Node.getNode("i"), Node.getNode("i"), true));
+        edges.add(new Edge(Node.getNode("i"), Node.getNode("g"), true));
+        edges.add(new Edge(Node.getNode("j"), Node.getNode("k"), true));
+        edges.add(new Edge(Node.getNode("j"), Node.getNode("c"), true));
+        edges.add(new Edge(Node.getNode("j"), Node.getNode("a"), true));
+        edges.add(new Edge(Node.getNode("j"), Node.getNode("b"), true));
+        edges.add(new Edge(Node.getNode("k"), Node.getNode("c"), true));
+        edges.add(new Edge(Node.getNode("k"), Node.getNode("g"), true));
+        edges.add(new Edge(Node.getNode("k"), Node.getNode("d"), true));
 
         GraphModel graph = new GraphModel(nodes, edges);
+        graph = GraphConverter.getUndirectedGraphModel(graph);
+        GraphVisualizer.displayGraph(graph);
 
-        System.setProperty("org.graphstream.ui", "javafx");
-
-        Graph graphViz = new SingleGraph("GKA Praktikum");
-
-        graph.getNodes().stream().forEach(node -> {
-            graphViz.addNode(node.getName());
-        });
-
-        graph.getEdges().stream().forEach(edge -> {
-            String idFrom = edge.getStart().getName();
-            String idTo = edge.getEnd().getName();
-            try {
-                graphViz.addEdge(idFrom+idTo, idFrom, idTo);
-            } catch (EdgeRejectedException exception) {
-                System.out.println(exception.getMessage());
-            }
-        });
-
-        graphViz.nodes().forEach(n -> n.setAttribute("ui.label", n.getId()));
-
-        graphViz.setAttribute("ui.stylesheet",
-                "node { fill-color: #1E88E5; size: 20px; text-alignment: above; text-size: 14px; } " +
-                        "edge { fill-color: #999; size: 2px; }");
-
-        graphViz.display();
+        BreadthFirstSearch.search(graph, Node.getNode("a"), Node.getNode("b"));
     }
 }
