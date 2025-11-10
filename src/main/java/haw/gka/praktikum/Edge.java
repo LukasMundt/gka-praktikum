74e4811bd15802f786f365cc4e989150a28c0ca8
Lukas Mundt <mail@lukas-mundt.de>
2025-11-10 17:45:15 +0100

    add method about the nodes to the edge and test them

--- src/main/java/haw/gka/praktikum/Edge.java
+++ src/main/java/haw/gka/praktikum/Edge.java
@@ -52,4 +52,46 @@ public class Edge {
     public Edge getUndirected() {
         return new Edge(_start, _end, false, _weight);
     }
+
+    public boolean isBReachableFromA(Node a, Node b) {
+        if (a == null || b == null) {
+            throw new IllegalArgumentException("a == null || b == null");
+        } else if(!a.equals(_start) && !a.equals(_end)){
+            throw new IllegalArgumentException("The node a is not part of this edge.");
+        } else if(!b.equals(_start) && !b.equals(_end)){
+            throw new IllegalArgumentException("The node b is not part of this edge.");
+        }
+
+        if(this._isDirected) {
+            return this._start.equals(a) && this._end.equals(b);
+        }
+        return this._start.equals(a) && this._end.equals(b) ||
+                this._end.equals(a) && this._start.equals(b);
+    }
+
+    public boolean isOtherNodeReachableFromA(Node a) {
+        if(a == null){
+            throw new IllegalArgumentException("a == null");
+        } else if(!a.equals(_start) && !a.equals(_end)){
+            return false;
+        }
+
+        if(this._isDirected) {
+            return a.equals(_start);
+        }
+        return this._start.equals(a) || this._end.equals(a);
+    }
+
+    public Node getOtherNode(Node node) {
+        if(node == null){
+            throw new IllegalArgumentException("node == null");
+        } else if(!node.equals(_start) && !node.equals(_end)){
+            throw new IllegalArgumentException("The node is not part of this edge.");
+        }
+
+        if(node.equals(_start)){
+            return _end;
+        }
+        return _start;
+    }
 }
---
+++ src/test/java/haw/gka/praktikum/EdgeTest.java
@@ -0,0 +1,50 @@
+package haw.gka.praktikum;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EdgeTest {
+    @Test
+    public void testUndirectedNeighbourTest(){
+        Node a = new Node("start");
+        Node b = new Node("end");
+        Edge edge = new Edge(a, b, false);
+
+        assertTrue(edge.isBReachableFromA(a, b));
+        assertTrue(edge.isBReachableFromA(b, a));
+
+        assertTrue(edge.isOtherNodeReachableFromA(a));
+        assertTrue(edge.isOtherNodeReachableFromA(b));
+    }
+
+    @Test
+    public void testDirectedNeighbourTest(){
+        Node a = new Node("start");
+        Node b = new Node("end");
+        Edge edge = new Edge(a, b, true);
+
+        assertTrue(edge.isBReachableFromA(a, b));
+        assertFalse(edge.isBReachableFromA(b, a));
+
+        assertTrue(edge.isOtherNodeReachableFromA(a));
+        assertFalse(edge.isOtherNodeReachableFromA(b));
+    }
+
+    @Test
+    public void testGetOtherNodeTest(){
+        Node a = new Node("start");
+        Node b = new Node("end");
+        Edge edge = new Edge(a, b, false);
+
+        assertEquals(b, edge.getOtherNode(a));
+        assertEquals(a, edge.getOtherNode(b));
+
+        // directed
+        Node aDir = new Node("start");
+        Node bDir = new Node("end");
+        Edge edgeDir = new Edge(aDir, bDir, true);
+
+        assertEquals(bDir, edgeDir.getOtherNode(aDir));
+        assertEquals(aDir, edgeDir.getOtherNode(bDir));
+    }
+}
