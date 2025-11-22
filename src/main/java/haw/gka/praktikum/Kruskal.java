5eb5644002957e5cc60c44a6e6cf9a57de121c75
Berenike <berenike.kuhli@haw-hamburg.de>
2025-11-22 18:47:51 +0100

    initial setup für Kruskal-Algorithmus, WIP

---
+++ src/main/java/haw/gka/praktikum/Kruskal.java
@@ -0,0 +1,21 @@
+package haw.gka.praktikum;
+
+/**
+ * Algorithmus von Kruskal:
+ * Eingabe: Eine Menge E der Kanten mit ihren Längen.
+ * Ausgabe: Eine Teilmenge F der Kantenmenge E
+ * <p>
+ * - Nummeriere die Kanten e1...e|E| nach steigender Länge. Setze F := {}
+ * - Für i := 1,..., |E|:
+ * Falls F vereinigt mit {ei} nicht die Kantenmenge eines Kreises in G
+ * enthält, setze F := F vereinigt mit {ei}
+ */
+public class Kruskal {
+    /**
+     * aus der Aufgabe:
+     * - Algorithmus implementieren und testen
+     * - Laufzeit ermitteln (kurz gewinnt)
+     * - Ergebnis: minimaler Spannbaum und sein Gesamtgewicht
+     * - JUnit-Tests, die Alg. mit Graphen aus Generator testen (umfassend)
+     */
+}
---
+++ src/test/java/haw/gka/praktikum/KruskalTest.java
@@ -0,0 +1,9 @@
+package haw.gka.praktikum;
+
+public class KruskalTest {
+    /** - JUnit-Tests, die Alg. mit Graphen aus Generator testen (umfassend)
+     *  - jede Methode aus Kruskal
+     *  - pos. und neg. Tests
+     *
+     */
+}
