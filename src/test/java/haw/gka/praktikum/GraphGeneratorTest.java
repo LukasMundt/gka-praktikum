980c80632c1c93e6357ecb14c742710f6b430401
Lukas Mundt <mail@lukas-mundt.de>
2026-01-04 03:18:41 +0100

    add tests for connected graphs and graphs where all node degrees should be even

--- src/test/java/haw/gka/praktikum/GraphGeneratorTest.java
+++ src/test/java/haw/gka/praktikum/GraphGeneratorTest.java
@@ -2,6 +2,8 @@ package haw.gka.praktikum;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GraphGeneratorTest {
@@ -19,19 +21,17 @@ public class GraphGeneratorTest {
         GraphModel genGraph2 = generator.generateGraph(30, 5);
         GraphModel genGraph3 = generator.generateGraph(3000, 5000);
 
-        assertEquals(genGraph.getEdges().size(), 3);
-        assertNotEquals(genGraph.getEdges().size(), 2);
-        assertNotEquals(genGraph.getEdges().size(), 4);
-        assertEquals(genGraph.getNodes().size(), 3);
-        assertNotEquals(genGraph.getNodes().size(), 4);
+        assertEquals(3, genGraph.getEdges().size());
+        assertNotEquals(2, genGraph.getEdges().size());
+        assertNotEquals(4, genGraph.getEdges().size());
+        assertEquals(3, genGraph.getNodes().size());
+        assertNotEquals(4, genGraph.getNodes().size());
 
-        assertEquals(genGraph2.getNodes().size(), 30);
-        assertEquals(genGraph2.getEdges().size(), 5);
+        assertEquals(30, genGraph2.getNodes().size());
+        assertEquals(5, genGraph2.getEdges().size());
 
-        assertEquals(genGraph3.getNodes().size(), 3000);
-        assertEquals(genGraph3.getEdges().size(), 5000);
-
-
+        assertEquals(3000, genGraph3.getNodes().size());
+        assertEquals(5000, genGraph3.getEdges().size());
     }
 
     /**
@@ -47,6 +47,46 @@ public class GraphGeneratorTest {
         assertThrows(IllegalArgumentException.class,
                 () -> generator.generateGraph(2, 3));
 
+    }
+
+    @Test
+    void testVollstaendig() {
+        GraphModel genGraph = generator.generateGraph(4, 6);
+        assertEquals(6, genGraph.getEdges().size());
+
+        GraphModel genGraph2 = generator.generateGraph(30, 435, false, true);
+        assertEquals(30, genGraph2.getNodes().size());
+        assertEquals(435, genGraph2.getEdges().size());
+    }
+
+    @Test
+    void testConntected() {
+        GraphModel genGraph1 = generator.generateGraph(30, 304, false, true);
+        assertEquals(30, genGraph1.getNodes().size());
+        assertEquals(304, genGraph1.getEdges().size());
+        assertTrue(genGraph1.isGraphConnected());
+
+        GraphModel genGraph2 = generator.generateGraph(30, 306, false, true);
+        assertEquals(30, genGraph2.getNodes().size());
+        assertEquals(306, genGraph2.getEdges().size());
+        assertTrue(genGraph2.isGraphConnected());
+    }
+
+    @Test
+    void testAllNodeDegreesEven() {
+        GraphModel genGraph = generator.generateGraph(30, 306, false, true);
+        assertEquals(306, genGraph.getEdges().size());
+
+        for(Set<Edge> edgesOfNode : genGraph.getAdjacency().values()) {
+            int degree = edgesOfNode.size();
+
+            for (Edge edge : edgesOfNode) {
+                if(edge.getStart().equals(edge.getEnd())) {
+                    degree++;
+                }
+            }
+            assertFalse(degree % 2 != 0);
+        }
     }
 
 }
