6d127730e02f30e82b75962dce0a87f6eb4d0092
Lukas Mundt <mail@lukas-mundt.de>
2026-01-05 16:32:45 +0100

    add tests for fleury

---
+++ src/test/java/haw/gka/praktikum/FleuryTest_generatedGraphs.java
@@ -0,0 +1,145 @@
+package haw.gka.praktikum;
+
+import haw.gka.praktikum.euler.Euler;
+import haw.gka.praktikum.euler.Fleury;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * - JUnit-Tests, die Alg. mit großen generierten Graphen testen (umfassend)
+ * - pos. und neg. Tests
+ */
+public class FleuryTest_generatedGraphs {
+    GraphGenerator generator = new GraphGenerator();
+
+    //erweiterter Generator mit kleinem, überschaubarem Graph testen
+    @Test
+    void test_modifiedGenerator_evenDegrees() throws IOException {
+        GraphModel genGraph = generator.generateGraph(8, 12, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    /**
+     * ab hier die vorgegebenen Werte für die generierten Graphen aus der
+     * Aufgabe
+     *
+     * @throws IOException
+     */
+    @Test
+    void test_100edges_32nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(32, 100, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    @Test
+    void test_300edges_64nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(64, 300, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    @Test
+    void test_1000edges_128nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(128, 1000, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    @Test
+    void test_3000edges_256nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(256, 3000, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    @Test
+    void test_10_000edges_512nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(512, 10_000, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    @Test
+    void test_30_000edges_1024nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(1024, 30_000, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    @Test
+    void test_100_000edges_2048nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(2048, 100_000, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    @Test
+    void test_300_000edges_4096nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(4096, 300_000, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    @Test
+    void test_1_000_000edges_8192nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(8192, 1_000_000, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    @Test
+    void test_3_000_000edges_16_384nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(16_384, 3_000_000, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+
+    @Test
+    void test_10_000_000edges_32_768nodes() throws IOException {
+        GraphModel genGraph = generator.generateGraph(32_768, 10_000_000, true, true);
+
+        Fleury fleury = Fleury.search(genGraph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(Euler.checkEulerCircle(genGraph, actual_eulerkreis));
+    }
+}
---
+++ src/test/java/haw/gka/praktikum/FleuryTest_smallGraphs.java
@@ -0,0 +1,126 @@
+package haw.gka.praktikum;
+
+import haw.gka.praktikum.euler.Euler;
+import haw.gka.praktikum.euler.Fleury;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static haw.gka.praktikum.euler.Euler.checkEulerCircle;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * - JUnit-Tests, die Alg. mit kleinen Graphen aus Dateien
+ * testen (umfassend)
+ * - pos. und neg. Tests
+ */
+
+public class FleuryTest_smallGraphs {
+
+    GraphIn graphReader = new GraphIn();
+    //folgende Pfade führen zu Graphen mit Eulerkreis
+    String path_eulercircle = "src/test/java/resources/euler/Eulerkreis.gka";
+    String path_euler_cities = "src/test/java/resources/euler/cities.gka";
+    String path_euler_sorted = "src/test/java/resources/euler" +
+            "/Euler_already_sorted.gka";
+    String path_euler_minimum = "src/test/java/resources/euler/minimum.gka";
+
+    //folgende Pfade führen zu Graphen ohne Eulerkreis
+    String path_unevenDegrees = "src/test/java/resources" +
+            "/undirected.gka";
+    String path_evenDegrees_unconnected = "src/test/java/resources/euler" +
+            "/keinEulerkreis.gka";
+    String path_unconnected_solo = "src/test/java/resources/euler" +
+            "/TestFileSolo.gka";
+    String path_directed = "src/test/java/resources/euler/directed.gka";
+    String path_too_small = "src/test/java/resources/euler/smallGraph.gka";
+
+
+    @Test
+    void testSearchEulerCircle_Error_GraphIsDirected() throws IOException {
+        GraphModel directed = graphReader.readGraph(path_directed);
+
+
+        assertThrows(IllegalArgumentException.class,
+                () -> Fleury.search(directed));
+    }
+
+    @Test
+    void testSearchEulerCircle_Error_graphIsTooSmall() throws IOException {
+        GraphModel tooSmall = graphReader.readGraph(path_too_small);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> Fleury.search(tooSmall));
+    }
+
+    @Test
+    void testSearchEulerCircle_Error_GraphIsUnconnected() throws IOException {
+        GraphModel unconnected = graphReader.readGraph(path_evenDegrees_unconnected);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> Fleury.search(unconnected));
+    }
+
+    @Test
+    void testSearchEulerCircle_Error_GraphHasSoloNodes() throws IOException {
+        GraphModel unconnected_solo =
+                graphReader.readGraph(path_unconnected_solo);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> Fleury.search(unconnected_solo));
+    }
+
+    @Test
+    void testSearchEulerCircle_Error_GraphHasOddDegrees() throws IOException {
+        GraphModel oddDegrees = graphReader.readGraph(path_unevenDegrees);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> Fleury.search(oddDegrees));
+    }
+
+    @Test
+    void testThrowsIOException() throws IOException {
+        assertThrows(IllegalArgumentException.class,
+                () -> Fleury.search(null));
+    }
+
+    /**
+     * Die folgenden Tests testen den tatsächlichen Algorithmus und sollen
+     * Eulerkreise finden
+     *
+     * @throws IOException
+     */
+    @Test
+    void testFindsEulerCircle_Small() throws IOException {
+        GraphModel graph = graphReader.readGraph(path_eulercircle);
+
+        Fleury fleury = Fleury.search(graph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(checkEulerCircle(graph, actual_eulerkreis));
+    }
+
+    @Test
+    void testFindsEulerCircle_AlreadySorted() throws IOException {
+        GraphModel graph = graphReader.readGraph(path_euler_sorted);
+
+        Fleury fleury = Fleury.search(graph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(checkEulerCircle(graph, actual_eulerkreis));
+    }
+
+    @Test
+    void testFindsEulerCircle_Nodenames_Weights() throws IOException {
+        GraphModel graph = graphReader.readGraph(path_euler_cities);
+
+        Fleury fleury = Fleury.search(graph);
+        List<Edge> actual_eulerkreis = fleury.getEulerCircleTour();
+
+        assertTrue(checkEulerCircle(graph, actual_eulerkreis));
+    }
+    //TODO noch weitere Randfälle?
+}
