3a5d70fbeb5894e78e33065cf11ca82b2a240a5e
Berenike <berenike.kuhli@haw-hamburg.de>
2026-01-19 21:26:52 +0100

    update HierholzerTest_generatedGraphs, all pass except the biggest one (too big)

--- src/test/java/haw/gka/praktikum/HierholzerTest_generatedGraphs.java
+++ src/test/java/haw/gka/praktikum/HierholzerTest_generatedGraphs.java
@@ -1,11 +1,14 @@
 package haw.gka.praktikum;
 
 import haw.gka.praktikum.euler.Euler;
+import haw.gka.praktikum.euler.Hierholzer;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static haw.gka.praktikum.euler.Hierholzer.searchEulerCircle;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -19,10 +22,10 @@ public class HierholzerTest_generatedGraphs {
     @Test
     void test_modifiedGenerator_evenDegrees() throws IOException {
         GraphModel genGraph = generator.generateGraph(8, 12, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     /**
@@ -34,99 +37,100 @@ public class HierholzerTest_generatedGraphs {
     @Test
     void test_100edges_32nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(32, 100, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertNotNull(actual_eulerkreis);
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     @Test
     void test_300edges_64nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(64, 300, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-//        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     @Test
     void test_1000edges_128nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(128, 1000, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-//        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     @Test
     void test_3000edges_256nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(256, 3000, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-//        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     @Test
     void test_10_000edges_512nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(512, 10_000, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-//        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     @Test
     void test_30_000edges_1024nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(1024, 30_000, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-//        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     @Test
     void test_100_000edges_2048nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(2048, 100_000, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-//        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     @Test
     void test_300_000edges_4096nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(4096, 300_000, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-//        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     @Test
     void test_1_000_000edges_8192nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(8192, 1_000_000, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-//        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     @Test
     void test_3_000_000edges_16_384nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(16_384, 3_000_000, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-//        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 
     @Test
     void test_10_000_000edges_32_768nodes() throws IOException {
         GraphModel genGraph = generator.generateGraph(32_768, 10_000_000, true, true);
-
+        List<Edge> edgeList = Hierholzer.executeHierholzer(genGraph);
         GraphModel actual_eulerkreis = searchEulerCircle(genGraph);
 
-//        assertTrue(Euler.checkEulerCircle(actual_eulerkreis));
+        assertTrue(Euler.checkEulerCircle(actual_eulerkreis, edgeList));
     }
 }
